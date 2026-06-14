d8b49ded16202b94fe8d439077d98b5669318a53
luckydyu <luckydyu@gmail.com>
2026-06-14 18:18:23 +0900

    Introduce shared app state

---
+++ src/state.js
@@ -0,0 +1,14 @@
+window.SpaceTimerState = {
+  appState: 'SETUP',
+  targetLaps: 10,
+  currentLap: 1,
+  chosenCar: 'car-1',
+  chosenDestination: 'destination-1',
+  selectedCarCategory: '전체',
+  selectedDestinationCategory: '전체',
+  timerInterval: null,
+  lapStartTime: 0,
+  totalElapsedMs: 0,
+  currentLapElapsed: 0,
+  lapRecords: []
+};
